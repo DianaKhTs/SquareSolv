0519905323dbaca9d855a5af6c76f214c61e4544
DianaKhTs <111983177+DianaKhTs@users.noreply.github.com>
2022-08-24 22:20:26 +0300

    Update input_coeff.c

--- input_coeff.c
+++ input_coeff.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/*!
+    @brief  This function demands input of coefficients
+    @detailed Checks the right input
+    @return Returns nothing
+*/
 void Input(double* coef_a, double* coef_b, double* coef_c)
 {
+
     printf("Bведите 3 значения коэффициентов:\n");
     while (scanf("%lg  %lg  %lg", coef_a, coef_b, coef_c) != 3)
     {
