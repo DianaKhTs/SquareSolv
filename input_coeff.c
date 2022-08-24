6b40aede72d7d4f8d41f3492f11293a15e5612ef
DianaKhTs <111983177+DianaKhTs@users.noreply.github.com>
2022-08-24 22:21:09 +0300

    Update input_coeff.c

--- input_coeff.c
+++ input_coeff.c
@@ -7,7 +7,6 @@
 */
 void Input(double* coef_a, double* coef_b, double* coef_c)
 {
-
     printf("Bведите 3 значения коэффициентов:\n");
     while (scanf("%lg  %lg  %lg", coef_a, coef_b, coef_c) != 3)
     {
