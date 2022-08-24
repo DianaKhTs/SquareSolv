b76906e8c67117b008059db434a66a0056990c4e
DianaKhTs <111983177+DianaKhTs@users.noreply.github.com>
2022-08-24 22:04:22 +0300

    Update solve_eq.h

--- solve_eq.h
+++ solve_eq.h
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
 
+void Input(double* coef_a, double* coef_b, double* coef_c);
 int SolveEq(double a, double b, double c, double* p_x1, double* p_x2);
 void PrintAnswer(int number_of_roots, double* p_x1,  double* p_x2);
