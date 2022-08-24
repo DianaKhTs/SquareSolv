b77bf72cfc787f139de33a692aa59636da4ce994
DianaKhTs <111983177+DianaKhTs@users.noreply.github.com>
2022-08-24 22:02:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include "solve_eq.h"
 
-
-
 int main()
 {
     double coef_a = 0, coef_b = 0, coef_c = 0;
     double x1 = 0, x2 = 0;
 
-    while (scanf("%lg  %lg  %lg", &coef_a, &coef_b, &coef_c) != 3)
-    {
-       // printf("%lg  %lg  %lg", coef_a, coef_b, coef_c);
-        printf("error");
-        fflush(stdin);
-    }
-   
+    Input(&coef_a, &coef_b, &coef_c);
     PrintAnswer(SolveEq(coef_a, coef_b, coef_c, &x1, &x2), &x1, &x2);
 }
