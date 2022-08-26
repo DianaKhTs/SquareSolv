7c531079d13354e814e2f5d810531126f687496d
DianaKhTs <111983177+DianaKhTs@users.noreply.github.com>
2022-08-26 17:52:04 +0300

    Update solve_eq.h

--- solve_eq.h
+++ solve_eq.h
@@ -1,5 +1,9 @@
 
 #include <stdio.h>
+#include <stdio.h>
+#include <math.h>
+#include "assert.h"
+#include <time.h>
 
 void Input(double* coef_a, double* coef_b, double* coef_c);
 int SolveEq(double a, double b, double c, double* p_x1, double* p_x2);
